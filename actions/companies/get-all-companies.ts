de5de8c813dae90899f396159f3874f7be253f7a
haquesomrat <ihsomrat.official@gmail.com>
2024-10-06 02:35:16 +0600

    add cache no store to the get all companies

--- actions/companies/get-all-companies.ts
+++ actions/companies/get-all-companies.ts
@@ -1,6 +1,8 @@
 export const getAllCompanies = async () => {
   try {
-    const response = await fetch(`/dashboard/companies/api`);
+    const response = await fetch(`/dashboard/companies/api`, {
+      cache: "no-store",
+    });
     return response;
   } catch (error) {
     console.error("An error occurred while fetching companies:", error);
