31f8f099681dd074394ae86aea50c05ac14e2ca2
haquesomrat <ihsomrat.official@gmail.com>
2024-10-21 22:35:50 +0600

    updated projects api action

--- actions/projects/get-all-projects.ts
+++ actions/projects/get-all-projects.ts
@@ -1,6 +1,9 @@
 export const getAllProjects = async () => {
   try {
-    const response = await fetch(`/dashboard/projects/api`);
+    const response = await fetch(`/dashboard/projects/api`, {
+      cache: "no-store",
+      next: { revalidate: 3600 },
+    });
     return response;
   } catch (error) {
     console.error("An error occurred while fetching:", error);
